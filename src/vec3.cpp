8d755066ce9070eff4374d885d6964862f197e49
devsifi <devsfiq@gmail.com>
2020-03-07 23:44:32 +0800

    Added Vec3 Implementation

---
+++ src/vec3.cpp
@@ -0,0 +1,161 @@
+#include "vec3.h"
+
+#include <cmath>
+
+#pragma region  MEMBER FUNCTIONS
+
+float vec3::length() const 
+{
+    return std::sqrt(
+        x * x +
+        y * y +
+        z * z
+    );
+}
+
+
+float vec3::length_squared() const 
+{
+    return  x * x +
+            y * y +
+            z * z;
+}
+
+#pragma endregion
+
+#pragma region  NON-MEMBER FUNCTIONS
+
+float dot(const vec3 &a, const vec3 &b)
+{
+    return  a.x * b.x +
+            a.y * b.y +
+            a.z * b.z;
+}
+
+vec3 normalise(const vec3 &vec)
+{
+    return vec / vec.length();
+}
+
+vec3 cross(const vec3 &a, const vec3 &b)
+{
+    return { 
+        a.y  * b.z - a.z * b.y,
+        a.z  * b.x - a.x * b.z,
+        a.x  * b.y - a.y * b.x,
+    };
+}
+
+#pragma endregion
+
+#pragma region  OPERATOR OVERLOADING FUNCTIONS
+
+vec3 operator+(const vec3 &a, const vec3 &b)
+{
+    return {
+        a.x + b.x, 
+        a.y + b.y, 
+        a.z + b.z
+    };
+}
+
+vec3 operator-(const vec3 &a, const vec3 &b)
+{
+    return {
+        a.x - b.x, 
+        a.y - b.y, 
+        a.z - b.z
+    };
+}
+
+vec3 operator*(const vec3 &a, const vec3 &b)
+{
+    return {
+        a.x * b.x,
+        a.y * b.y,
+        a.z * b.z
+    };
+}
+
+vec3 operator/(const vec3 &a, const vec3 &b)
+{
+    return {
+        a.x / b.x,
+        a.y / b.y,
+        a.z / b.z
+    };
+}
+
+vec3 operator+(const vec3 &vec, const float f)
+{
+    return {
+        vec.x + f, 
+        vec.y + f, 
+        vec.z + f
+    };
+}
+
+vec3 operator-(const vec3 &vec, const float f)
+{
+    return {
+        vec.x - f,
+        vec.y - f,
+        vec.z - f
+    };
+}
+
+vec3 operator*(const vec3 &vec, const float f)
+{
+    return {
+        vec.x * f,
+        vec.y * f, 
+        vec.z * f
+    };
+}
+
+vec3 operator/(const vec3 &vec, const float f)
+{
+    return {
+        vec.x / f, 
+        vec.y / f, 
+        vec.z / f
+    };
+}
+
+vec3 operator+(const float f, const vec3 &vec)
+{
+    return {
+        vec.x + f, 
+        vec.y + f, 
+        vec.z + f
+    };
+}
+
+vec3 operator-(const float f, const vec3 &vec)
+{
+    return {
+        vec.x - f, 
+        vec.y - f, 
+        vec.z - f
+    };
+}
+
+vec3 operator*(const float f, const vec3 &vec)
+{
+    return {
+        vec.x * f,
+        vec.y * f,
+        vec.z * f
+    };
+}
+
+vec3 operator/(const float f, const vec3 &vec)
+{
+    return {
+        vec.x / f, 
+        vec.y / f, 
+        vec.z / f
+    };
+}
+
+#pragma endregion
---
+++ src/vec3.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdexcept>
+#include <cmath>
+
+struct vec3
+{
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+
+    float length() const;
+    float length_squared() const;
+};
+
+float dot(const vec3 &, const vec3 &);
+
+vec3 normalise(const vec3 &);
+vec3 cross(const vec3 &, const vec3 &);
+
+vec3 operator+(const vec3 &, const vec3 &);
+vec3 operator-(const vec3 &, const vec3 &);
+vec3 operator*(const vec3 &, const vec3 &);
+vec3 operator/(const vec3 &, const vec3 &);
+
+vec3 operator+(const vec3 &, float);
+vec3 operator-(const vec3 &, float);
+vec3 operator*(const vec3 &, float);
+vec3 operator/(const vec3 &, float);
+
+vec3 operator+(float, const vec3 &);
+vec3 operator-(float, const vec3 &);
+vec3 operator*(float, const vec3 &);
+vec3 operator/(float, const vec3 &);
